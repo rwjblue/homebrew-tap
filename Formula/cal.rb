3503697f46311f726d40a4428888a16fc207efe9
Robert Jackson <me@rwjblue.com>
2024-05-04 13:40:29 -0400

    Update cal to v0.2.0

--- Formula/cal.rb
+++ Formula/cal.rb
@@ -1,21 +1,21 @@
 class Cal < Formula
   desc "Simple command line calendar, written in Rust"
   homepage "https://github.com/rwjblue/cal-rs"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rwjblue/cal-rs/releases/download/v0.1.0/cal-aarch64-apple-darwin.tar.gz"
-      sha256 "60b625b5f2c77e45a897a4434eb79dc22c4614c7631b2c22f8f497ba59998868"
+      url "https://github.com/rwjblue/cal-rs/releases/download/v0.2.0/cal-aarch64-apple-darwin.tar.gz"
+      sha256 "e4938512f88e96f7e6ca2ca2f3286b22e8441aeff8af5b8178c636d529ac5fee"
     else
-      url "https://github.com/rwjblue/cal-rs/releases/download/v0.1.0/cal-x86_64-apple-darwin.tar.gz"
-      sha256 "934a0a8aaef26847d84ad30ded206b8063a0585f754dde46982de84ba7ee81ac"
+      url "https://github.com/rwjblue/cal-rs/releases/download/v0.2.0/cal-x86_64-apple-darwin.tar.gz"
+      sha256 "9d3de779c9e70cfae0f95ce6d7958234b6e3d9ccf30514e798aaec4323cc47d4"
     end
   end
   on_linux do
-    url "https://github.com/rwjblue/cal-rs/releases/download/v0.1.0/cal-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "fa70c1d402bf6b8c5e67cbdb840382fe8eceb7d961fe2679efc73cc1599e5726"
+    url "https://github.com/rwjblue/cal-rs/releases/download/v0.2.0/cal-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "0ac8087583588eff2241fb8717e38fa945780951a486283e1b5d25cfb4321abf"
   end
 
   def install
